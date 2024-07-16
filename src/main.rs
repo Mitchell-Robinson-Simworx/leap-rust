65faf8a6aab53ddb50c1e9eb7bee7e18f1889232
Mitch.R <mitchellrobinson@simworx.co.uk>
2024-07-16 11:21:38 +0100

    ASDASDA

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::result;
+
+use clap::Parser;
+
+//cargo r -- some-pattern some-file
+#[derive(Parser)]
+struct Cli {
+    year: i32,
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    if (args.year % 4 == 0 && args.year % 100 != 0 || args.year % 400 == 0) {
+        println!("The year {} is a leap year", args.year);
+    } else {
+        println!("The year {} is not a leap year", args.year);
+    }
+}
